f9d9f16df592f1f4cdbbe015a5cd77e79c77032e
DanZC <officialdanzc@gmail.com>
2024-06-16 20:05:07 -0400

    gfx/pokemon/suicune fixed

---
+++ gfx/pokemon/suicune/anim.h
@@ -0,0 +1,14 @@
+static const uint8_t SuicuneAnimation[] = {
+    frame2(1, 10),
+    frame2(2, 10),
+    frame2(3, 10),
+    frame2(4, 10),
+    frame2(5, 10),
+    setrepeat(2),
+    frame2(6,  3),
+    frame2(7,  3),
+    frame2(8,  3),
+    frame2(0,  3),
+    dorepeat(6),
+    endanim,
+};
---
+++ gfx/pokemon/suicune/anim_idle.h
@@ -0,0 +1,8 @@
+static const uint8_t SuicuneAnimationIdle[] = {
+    frame2(9, 35),
+    setrepeat(2),
+    frame2(0,  6),
+    frame2(9,  6),
+    dorepeat(2),
+    endanim,
+};
