3875cfda8fae8ea93d4a048a66eef3b6052a5f2f
DanZC <officialdanzc@gmail.com>
2025-04-30 20:53:13 -0400

    Fixed windows GetNanoseconds function

--- util/hrc.c
+++ util/hrc.c
@@ -1,12 +1,12 @@
 #if !ENHANCEMENT_USE_PCG
-#include <stdint.h> // `UINT64_MAX`
+#include <stdint.h>
+#include <stdio.h>
 #include "./hrc.h"
 /// Convert seconds to nanoseconds
 #define SEC_TO_NS(sec) ((sec)*1000000000)
 
 #if defined(__unix__)
 #include <errno.h>  // `errno`
-#include <stdio.h>  // `printf()`
 #include <string.h> // `strerror(errno)`
 #include <time.h>   // `clock_gettime()` and `timespec_get()`
 
@@ -33,12 +33,18 @@ uint64_t GetNanoseconds(void){
 
 uint64_t GetNanoseconds(void){
     LARGE_INTEGER ticks;
+    LARGE_INTEGER freq;
     if (!QueryPerformanceCounter(&ticks))
     {
         printf("Failed to obtain timestamp.\n");
         return UINT64_MAX; // use this to indicate error
     }
-    return (uint64_t)(SEC_TO_NS(ticks.QuadPart / QueryPerformanceFrequency()));
+    if (!QueryPerformanceFrequency(&freq))
+    {
+        printf("Failed to obtain frequency.\n");
+        return UINT64_MAX; // use this to indicate error
+    }
+    return (uint64_t)(SEC_TO_NS((double)ticks.QuadPart / (double)freq.QuadPart));
 }
 #else
 #error "Unsupported platform?"
